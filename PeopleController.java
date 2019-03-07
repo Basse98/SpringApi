8870e771a96f2b977a6bc77ed8e4e1a7ab4ab1c7
Sebastian <sebastian.aronsson@iths.se>
2019-03-07 12:27:32 +0100

    Add PUT + DELETE methods

--- PeopleController.java
+++ PeopleController.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,14 +10,36 @@ import java.util.List;
 @RestController
 public class PeopleController {
 
-    @Autowired
+    //@Autowired
     private PeopleService peopleService;
+
+    public PeopleController(PeopleService service) {
+    this.peopleService = service;
+    }
 
     @RequestMapping(value = "/people")
     public List<Person> getAllPeople(){
         return peopleService.getAllPeople();
     }
 
+    @RequestMapping(value = "/people/{input}")
+    public Person getPerson(@PathVariable("input") int id){
+        return peopleService.getPerson(id);
+    }
 
+    @RequestMapping(value = "/people",method = RequestMethod.POST)
+    public void addPerson(@RequestBody Person person){
+        peopleService.addPerson(person);
+    }
+
+    @RequestMapping(value = "/people/{id}",method = RequestMethod.PUT)
+    public void updatePerson(@RequestBody Person person, @PathVariable int id){
+        peopleService.updatePerson(id,person);
+    }
+
+    @RequestMapping(value = "/people/{input}",method = RequestMethod.DELETE)
+    public void  deletePerson(@PathVariable("input") int id){
+        peopleService.deletePerson(id);
+    }
 
 }
--- PeopleService.java
+++ PeopleService.java
@@ -2,20 +2,42 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class PeopleService {
 
-    List<Person> people = Arrays.asList(
+    List<Person> people = new ArrayList<>(Arrays.asList(
             new Person(1,19,"Oskar"),
             new Person(2,20,"Basse"),
             new Person(3,18,"Monika")
-    );
+    ));
 
     public List<Person> getAllPeople(){
         return people;
     }
 
+    public Person getPerson(int id){
+        return people.stream().filter(i->i.getId() == id).findFirst().get();
+    }
+
+    public void addPerson(Person person) {
+        people.add(person);
+    }
+
+    public void updatePerson(int id, Person person) {
+        for (int i = 0; i < people.size(); i++) {
+            Person p = people.get(i);
+            if(p.getId() == id){
+                people.set(i,person);
+                return;
+            }
+        }
+    }
+
+    public void deletePerson(int id) {
+        people.removeIf(c -> c.getId() == id);
+    }
 }
